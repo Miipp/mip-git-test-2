602c99716c5c9b3079c49c2716ae2fb666f0c245
Miipp <miftahulokt@gmail.com>
2025-07-17 12:23:00 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam eveniet, voluptatum officiis atque repellendus fuga accusantium, sequi similique ea expedita provident facere libero a, illo nobis. Sequi nesciunt possimus eaque.</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
