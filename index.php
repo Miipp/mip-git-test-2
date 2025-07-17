da7fb8c0229d2a00956ac5a7ee5f75cf58175d08
Miipp <miftahulokt@gmail.com>
2025-07-17 12:17:49 +0700

    menambahkan paragraf lorem

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam eveniet, voluptatum officiis atque repellendus fuga accusantium, sequi similique ea expedita provident facere libero a, illo nobis. Sequi nesciunt possimus eaque.</p>
 </body>
 </html>
